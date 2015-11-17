8ea3ff58b96dfb8655469057efe55a4f882b4193
joshcheshire <joshuacheshire@hotmail.com>
2015-11-17 12:42:08 -0700

    port

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', function(req,res){
 // req.params -> {heroName : 'gorilla'}
 
 // Creating Server and Listening for Connections \\
-var port = 3000
+var port = 80
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
