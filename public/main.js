d9677d61c1a80ab00d88052e5fc451e1dcad07ff
joshcheshire <joshuacheshire@hotmail.com>
2016-01-09 12:47:52 -0700

    update saturday

--- public/main.js
+++ public/main.js
@@ -14,7 +14,7 @@ angular.module('app')
     var homeControllerFunc = function($scope, $sce){
     		$scope.$sce= $sce;
 
-    	$scope.greeting = "To Keep Us Free"
+    	$scope.greeting = "The Indomitable Freedom Quest: Honoring America's Sacred Trust"
     	$scope.author = "About the Author"
     	$scope.imageSource="./test/pics/rdc.jpg"
     
